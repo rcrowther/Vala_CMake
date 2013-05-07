211f050ab66e9e602d88a6c45c98c77664b46ae5
rcrowther <r.crowther1@virginmedia.com>
2013-05-07 10:44:37 +0100

    new file:   vala/FindValaBinding.cmake 	modified:   vala/UseVala.cmake
    Add the new FindValaBinding macro.
    Update to the new UseVala macro
    (not compatible with previous versions).

---
+++ vala/FindValaBinding.cmake
@@ -0,0 +1,258 @@
+# - find Vala bindings and construct CFLAGS
+#
+# The module will guarentee named bindings exist under found directories.
+#
+# Vala bindings are not a direct map to bound code. They use their own
+# namespacing. When installed as a package, many bindings may be
+# included for uninstalled libraries. Cross-checking would rely on
+# intimate knowledge of the bindings, which seems.. tedious. So this
+# module makes no attempt to check if the bindings refer to installed
+# code. Before running this module, Cmake code can/should, by the
+# usual methods, check the underlying libraries exist.
+#
+# Like FindPkgConfig, this module caches found information. If paths, options  
+# (including FindValaLocations options), or system configurations are changed,
+# the build cache must be cleared for the changes to have an effect.
+#
+# This module relies on a previous call to FindValaBindingLocations.
+#
+# Usage:
+#   vala_check_binding(<GROUP_ID> [REQUIRED] [QUIET] <binding name> [<binding name>]*)
+#
+#
+# Takes some arguments resembling standard module:
+#   [REQUIRED] [QUIET]
+#
+# Takes arguments:
+#  GROUP_ID
+#    Namespacing for each invokation of the macro. Use a name previously
+#    defined in a FindValaBindingsLocations call (from that call, this macro
+#    knows where to look).
+#
+#
+# Defines:
+# Like FindPkgConfig, the following two variables are namespaced by the
+# supplied GROUP_ID (called PREFIX in FindPkgConfig). 
+# 
+#   <IGROUP_ID>_VALA_BINDINGS_FOUND
+#     (INTERNAL CACHED) Set to 1 if the bindings exist. If REQUIRED is set,
+#     *all* listed bindings must be found.
+#
+#   <GROUP_ID>_VALA_BINDINGS_CFLAGS
+#     (INTERNAL CACHED) flags for a Valac compiler i.e
+#     --pkg=XXX ---pkg=XXX etc.
+#
+#
+# Example:
+#
+#  vala_check_binding(BINDINGS1 REQUIRED posix gio-2.0 gtk+-2.0 gee-1.0)
+#
+
+
+#=============================================================================
+# Copyright 2013 Robert Crowther. All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 
+#    1. Redistributions of source code must retain the above copyright notice,
+#       this list of conditions and the following disclaimer.
+# 
+#    2. Redistributions in binary form must reproduce the above copyright notice,
+#       this list of conditions and the following disclaimer in the documentation
+#       and/or other materials provided with the distribution.
+# 
+# THIS SOFTWARE IS PROVIDED BY ROBERT CROWTHER ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+# EVENT SHALL JAKOB WESTHOFF OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# 
+# The views and conclusions contained in the software and documentation are those
+# of the authors and should not be interpreted as representing official policies,
+# either expressed or implied, of Robert Crowther
+#=============================================================================
+
+
+
+# TOCONSIDER: find_package_handle_standard_args() is not used because this 
+# module does not always search for, or require, versioning.
+# TODO: Search on other architectures than apt-get
+# TODO: No version checks?
+ 
+include(CMakeParseArguments)
+
+
+###  Common ###
+set(VALA_BINDINGS_VERSION 1)
+
+# uncomment the following line to get debug output for this file
+# set(_Vala_Binding_DEBUG True)
+
+# Unset and set given variables pretyped as CACHED and INTERNAL
+macro(_valabindings_unset var)
+  set(${var} "" CACHE INTERNAL "")
+endmacro()
+
+macro(_valabindings_set var value)
+  set(${var} ${value} CACHE INTERNAL "")
+endmacro()
+
+
+# Splits given arguments into options and a package list
+macro(_valabindings_parse_options _result _required _silent)
+  set(${_required} 0)
+  set(${_silent} 0)
+
+  foreach(_pkg ${ARGN})
+    if (_pkg STREQUAL "REQUIRED")
+      set(${_required} 1)
+    endif ()
+    if (_pkg STREQUAL "QUIET")
+      set(${_silent} 1)
+    endif ()
+  endforeach()
+
+  set(${_result} ${ARGN})
+  list(REMOVE_ITEM ${_result} "REQUIRED")
+  list(REMOVE_ITEM ${_result} "QUIET")
+endmacro()
+
+
+function(_vala_check_bindings_internal)
+
+  cmake_parse_arguments(ARGS
+    ""
+    "REQUIRED;QUIET;GROUP_ID;"
+    "BINDINGS"
+    ${ARGN}
+    )
+
+  _valabindings_unset("${ARGS_GROUP_ID}_VALA_BINDING_FOUND")
+  _valabindings_unset("${${ARGS_GROUP_ID}_VALA_BINDINGS_CFLAGS}")
+
+  list(APPEND vala_binding_directories
+    ${${ARGS_GROUP_ID}_GENERIC_DIR}
+    ${${ARGS_GROUP_ID}_VERSIONED_DIR}
+    ${${ARGS_GROUP_ID}_CUSTOM_DIR_1}
+    ${${ARGS_GROUP_ID}_CUSTOM_DIR_2}
+    ${${ARGS_GROUP_ID}_CUSTOM_DIR_3}
+    ${${ARGS_GROUP_ID}_CUSTOM_DIR_4}
+    )
+
+
+  list(LENGTH vala_binding_directories _vala_directories_count)
+
+  if(_vala_directories_count EQUAL 0)
+    if (DEFINED ARGS_REQUIRED)
+      message(SEND_ERROR "No Vala binding directories found")
+    endif ()
+  else()
+
+    list(LENGTH ARGS_BINDINGS _vala_bindings_count)
+
+    # Status message of intentions
+    if (NOT ARGS_QUIET)
+      if (_vala_bindings_count EQUAL 1)
+        message(STATUS "checking for binding '${ARGS_BINDINGS}'")
+      else()
+        message(STATUS "checking for bindings '${ARGS_BINDINGS}'")
+      endif()
+    endif()
+
+    set(_vala_check_found_bindings)
+    set(_vala_check_bindings_no_fails true)
+
+    # iterate through the binding list
+    foreach (_vala_binding ${ARGS_BINDINGS})
+      set(_binding_exists false)
+
+      # execute the query
+      foreach (_vala_binding_dir ${vala_binding_directories})
+        if(EXISTS "${_vala_binding_dir}/${_vala_binding}.vapi")
+          set(_binding_exists true)
+          list(APPEND _vala_check_found_bindings ${_vala_binding})
+        endif()
+      endforeach()
+
+      # Check for failures
+      if (NOT _binding_exists)
+        if(NOT ARGS_QUIET)
+          message(STATUS "  binding '${_vala_binding}' not found")
+        endif()
+
+        set(_vala_check_bindings_no_fails false)
+      endif()
+    endforeach()
+
+
+    if(NOT _vala_check_bindings_no_fails)
+      # fail when requested
+      if (${ARGS_REQUIRED})
+        message(SEND_ERROR "Binding requirements failed. See the message output from the build")
+      endif()
+    else()
+
+      # On success, set variables
+      _valabindings_set(${ARGS_GROUP_ID}_VALA_BINDINGS_FOUND 1)
+ 
+      foreach (_binding ${_vala_check_found_bindings})
+        list(APPEND _vala_bindings_cflags "--pkg=${_binding}")
+        if(NOT ARGS_QUIET)
+          message(STATUS "  found ${_binding}")
+        endif()
+      endforeach()
+
+      _valabindings_set(${ARGS_GROUP_ID}_VALA_BINDINGS_CFLAGS
+        "${_vala_bindings_cflags}"
+        )
+    endif()
+
+  endif()
+endfunction()
+
+
+
+#
+# Main function
+#
+function(vala_check_binding _group_id)
+  cmake_parse_arguments(ARGS
+    "REQUIRED;QUIET"
+    ""
+    ""
+    ${ARGN}
+    )
+
+  if(NOT ${_group_id}_VALA_BINDINGS_FOUND)
+  if(NOT ${_group_id}_LOCATIONS_FOUND)
+    if (${_required})
+      message(SEND_ERROR "No Vala binding directories supplied. These should have been provided by the FindValaBindingLocations module?")
+    endif ()
+  else()
+
+    #set(required_ok TRUE)
+    #message(STATUS "ARGS_UNPARSED_ARGUMENTS: ${ARGS_UNPARSED_ARGUMENTS}")
+    _vala_check_bindings_internal(
+      REQUIRED ${ARGS_REQUIRED}
+      QUIET ${ARGS_QUIET}
+      GROUP_ID ${_group_id}
+      BINDINGS ${ARGS_UNPARSED_ARGUMENTS}
+      )
+  endif()
+  endif()
+
+  if (_Vala_Binding_DEBUG)
+    message(STATUS "--------FindValaBinding.cmake debug------------")
+    message(STATUS "${_group_id}_VALA_BINDINGS_FOUND: ${${_group_id}_VALA_BINDINGS_FOUND}")
+    message(STATUS "${_group_id}_VALA_BINDINGS_CFLAGS: ${${_group_id}_VALA_BINDINGS_CFLAGS}")
+    message(STATUS "--------------------")
+  endif()
+
+endfunction()
+
--- vala/UseVala.cmake
+++ vala/UseVala.cmake
@@ -1,31 +1,43 @@
-##
-# Compile vala files to their c equivalents for further processing. 
+# - Compile vala files to their c equivalents for further processing
 #
 # The "vala_precompile" function takes care of calling the valac executable on
-# the given source to produce c files which can then be processed further using
-# default cmake functions.
+# the given sources to produce c files which can then be processed further
+# using default cmake functions.
 #
 # The first parameter provided is a variable, which will be filled with a list
 # of c files outputted by the vala compiler. This list can than be used in
-# conjuction with functions like "add_executable" or others to create the
-# neccessary compile rules with CMake.
+# conjunction with functions like "add_executable" or others to create the
+# necessary compile rules with CMake.
+#
+# Valac must be provided with every file belonging to a target in the
+# project. Otherwise it can not resolve dependancies. This means it must
+# usually be run once only for each target.
+#
+# The need to run valac once and once only means that, if source data is
+# gathered from several areas (subdirectries, say) the data must be in full
+# path form. Otherwise, the singular invokation of valac can not trace the
+# files, as it has no knowledge of the directories used. This information
+# must be in the path. Please construct full path data before passing to this
+# function. 
+#
+# The current approach of this precompiler is to gather the valac-generated
+# c files into subdirectories of the build reflecting the source. If any
+# subdirectory sources are gathered, the macro add_subdirectory() will
+# generate the necessary directories in the build, so is a concise and safe
+# invokation (valac can segfualt if output directories are not present).
 #
 # The following sections may be specified afterwards to provide certain options
 # to the vala compiler:
 #
 # SOURCES
-#   A list of .vala files to be compiled. Please take care to add every vala
-#   file belonging to the currently compiled project or library as Vala will
-#   otherwise not be able to resolve all dependencies.
+#   A list of .vala files to be compiled. Must be in full path form, not
+#   filenames.
 #
-# PACKAGES
-#   A list of vala packages/libraries to be used during the compile cycle. The
-#   package names are exactly the same, as they would be passed to the valac
-#   "--pkg=" option.
-#
-# OPTIONS
-#   A list of optional options to be passed to the valac executable. This can be
-#   used to pass "--thread" for example to enable multi-threading support.
+# DIRECTORY
+#   Optionally specify an output directory for headers, vapis and c files. If
+#   this parameter is specified, it will become a subfolder of the current source
+#   directory. If unspecified, material output from valac is sent to the build
+#   folder.
 #
 # CUSTOM_VAPIS
 #   A list of custom vapi files to be included for compilation. This can be
@@ -42,25 +54,25 @@
 #   be a header file as well as an internal header file being generated called
 #   <provided_name>.h and <provided_name>_internal.h
 #
-# The following call is a simple example to the vala_precompile macro showing
-# an example to every of the optional sections:
+#  DEBUG
+#    Print debug information as the funtion is executed.
+#
+# The following call is a simple example to the vala_precompile() macro
+# showing an example to all the optional sections:
 #
 #   find_package(Vala "0.12" REQUIRED)
 #   inlcude(${VALA_USE_FILE})
+#
+#   vala_precompile_add_definitions("--thread")
+#   vala_precompile_add_definitions("--define=GTK2")
 #
 #   vala_precompile(VALA_C
 #     SOURCES
 #       source1.vala
 #       source2.vala
 #       source3.vala
-#     PACKAGES
-#       gtk+-2.0
-#       gio-1.0
-#       posix
-#     DIRECTORY
+#     OUTPUT_DIRECTORY
 #       gen
-#     OPTIONS
-#       --thread
 #     CUSTOM_VAPIS
 #       some_vapi.vapi
 #     GENERATE_VAPI
@@ -69,13 +81,21 @@
 #       myheader
 #     )
 #
+# Note the use of the macro
+#
+#  vala_precompile_add_definitions()
+#
+# to add detail to the valac call. This macro is in FindVala, so it can be
+# called throughout a CMakeLists.txt file, but it is here the data is used.
+#
 # Most important is the variable VALA_C which will contain all the generated c
-# file names after the call.
-##
+# file names after the call. These will be full-path files.
 
-##
+
+#=============================================================================
 # Copyright 2009-2010 Jakob Westhoff. All rights reserved.
 # Copyright 2010-2011 Daniel Pfeifer
+# Copyright 2013 Robert Crowther
 # 
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
@@ -101,80 +121,146 @@
 # The views and conclusions contained in the software and documentation are those
 # of the authors and should not be interpreted as representing official policies,
 # either expressed or implied, of Jakob Westhoff
-##
+#=============================================================================
+
+
+
+# TODO: Confirm GENERATE_HEADER;GENERATE_VAPI work
+
 
 include(CMakeParseArguments)
+cmake_policy(VERSION 2.8)
 
+###  Common ###
+set(VALA_USE_VERSION 1)
+
+# uncomment the following line to get debug output for this file
+# set(_Vala_Use_DEBUG True)
+
+
+
+#---------------------
+# Setup Configurations
+#---------------------
+# CMake does this automatically using internal methods, but we're in the DSL
+
+
+# TODO:Gumph. Should only happen on Make type builders, etc.
+if(CMAKE_BUILD_TYPE STREQUAL "Debug")
+  list(APPEND _VALA_PRECOMPILER_FLAGS ${CMAKE_VALA_FLAGS_DEBUG})
+elseif(CMAKE_BUILD_TYPE STREQUAL "Release")
+  list(APPEND _VALA_PRECOMPILER_FLAGS ${CMAKE_VALA_RELEASE})
+elseif(CMAKE_BUILD_TYPE STREQUAL "RelWithDebInfo")
+  list(APPEND _VALA_PRECOMPILER_FLAGS ${CMAKE_VALA_FLAGS_RELWITHDEBINFO})
+elseif(CMAKE_BUILD_TYPE STREQUAL "MinSizeRel")
+  list(APPEND _VALA_PRECOMPILER_FLAGS ${CMAKE_VALA_FLAGS_MINSIZEREL})
+endif()
+
+
+# Hack: Replace spaces in flag presets so they resemble lists
+# (...or when expanded the add_custom_command() will baulk because it can not
+# identify individual options)
+# Protect against the _VALA_PRECOMPILER_FLAGS = null, as the string method will
+# baulk too.
+if(_VALA_PRECOMPILER_FLAGS)
+  string(REPLACE " " ";" _VALA_PRECOMPILER_FLAGS ${_VALA_PRECOMPILER_FLAGS})
+endif()
+
+
+
+# TODO: Beats the debug option, but needs input setting as definitions.
+#function(vala_precompile_build_command)
+  # message(STATUS  "Valac execute: ${VALA_EXECUTABLE} -C ${header_arguments} ${vapi_arguments} -b ${CMAKE_CURRENT_SOURCE_DIR} -d ${OUTPUT_DIRECTORY} ${_VALA_PRECOMPILER_FLAGS} <some sources...>  ${custom_vapi_arguments}")
+  #message(STATUS  "Valac execute: ${VALA_EXECUTABLE} -C ${header_arguments} ${vapi_arguments} -b ${CMAKE_CURRENT_SOURCE_DIR} <some sources...>")
+#endfunction()
+
+
+#----------------
+# Vala Precompile
+#----------------
 function(vala_precompile output)
-    cmake_parse_arguments(ARGS "" "DIRECTORY;GENERATE_HEADER;GENERATE_VAPI"
-        "SOURCES;PACKAGES;OPTIONS;CUSTOM_VAPIS" ${ARGN})
-
-    if(ARGS_DIRECTORY)
-        set(DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/${ARGS_DIRECTORY})
-    else(ARGS_DIRECTORY)
-        set(DIRECTORY ${CMAKE_CURRENT_BINARY_DIR})
-    endif(ARGS_DIRECTORY)
-    include_directories(${DIRECTORY})
-    set(vala_pkg_opts "")
-    foreach(pkg ${ARGS_PACKAGES})
-        list(APPEND vala_pkg_opts "--pkg=${pkg}")
-    endforeach(pkg ${ARGS_PACKAGES})
-    set(in_files "")
-    set(out_files "")
-    foreach(src ${ARGS_SOURCES} ${ARGS_UNPARSED_ARGUMENTS})
-        list(APPEND in_files "${CMAKE_CURRENT_SOURCE_DIR}/${src}")
-        string(REPLACE ".vala" ".c" src ${src})
-        string(REPLACE ".gs" ".c" src ${src})
-        set(out_file "${DIRECTORY}/${src}")
-        list(APPEND out_files "${DIRECTORY}/${src}")
-    endforeach(src ${ARGS_SOURCES} ${ARGS_UNPARSED_ARGUMENTS})
-
-    set(custom_vapi_arguments "")
-    if(ARGS_CUSTOM_VAPIS)
-        foreach(vapi ${ARGS_CUSTOM_VAPIS})
-            if(${vapi} MATCHES ${CMAKE_SOURCE_DIR} OR ${vapi} MATCHES ${CMAKE_BINARY_DIR})
-                list(APPEND custom_vapi_arguments ${vapi})
-            else (${vapi} MATCHES ${CMAKE_SOURCE_DIR} OR ${vapi} MATCHES ${CMAKE_BINARY_DIR})
-                list(APPEND custom_vapi_arguments ${CMAKE_CURRENT_SOURCE_DIR}/${vapi})
-            endif(${vapi} MATCHES ${CMAKE_SOURCE_DIR} OR ${vapi} MATCHES ${CMAKE_BINARY_DIR})
-        endforeach(vapi ${ARGS_CUSTOM_VAPIS})
-    endif(ARGS_CUSTOM_VAPIS)
-
-    set(vapi_arguments "")
-    if(ARGS_GENERATE_VAPI)
-        list(APPEND out_files "${DIRECTORY}/${ARGS_GENERATE_VAPI}.vapi")
-        set(vapi_arguments "--internal-vapi=${ARGS_GENERATE_VAPI}.vapi")
-
-        # Header and internal header is needed to generate internal vapi
-        if (NOT ARGS_GENERATE_HEADER)
-            set(ARGS_GENERATE_HEADER ${ARGS_GENERATE_VAPI})
-        endif(NOT ARGS_GENERATE_HEADER)
-    endif(ARGS_GENERATE_VAPI)
-
-    set(header_arguments "")
-    if(ARGS_GENERATE_HEADER)
-        list(APPEND out_files "${DIRECTORY}/${ARGS_GENERATE_HEADER}.h")
-        list(APPEND out_files "${DIRECTORY}/${ARGS_GENERATE_HEADER}_internal.h")
-        list(APPEND header_arguments "--header=${DIRECTORY}/${ARGS_GENERATE_HEADER}.h")
-        list(APPEND header_arguments "--internal-header=${DIRECTORY}/${ARGS_GENERATE_HEADER}_internal.h")
-    endif(ARGS_GENERATE_HEADER)
-
-    add_custom_command(OUTPUT ${out_files} 
-    COMMAND 
-        ${VALA_EXECUTABLE} 
-    ARGS 
-        "-C" 
-        ${header_arguments} 
-        ${vapi_arguments}
-        "-b" ${CMAKE_CURRENT_SOURCE_DIR} 
-        "-d" ${DIRECTORY} 
-        ${vala_pkg_opts} 
-        ${ARGS_OPTIONS} 
-        ${in_files} 
-        ${custom_vapi_arguments}
-    DEPENDS 
-        ${in_files} 
-        ${ARGS_CUSTOM_VAPIS}
+  cmake_parse_arguments(ARGS
+    ""
+    "DIRECTORY;GENERATE_HEADER;GENERATE_VAPI"
+    "SOURCES;CUSTOM_VAPIS"
+    ${ARGN}
     )
-    set(${output} ${out_files} PARENT_SCOPE)
-endfunction(vala_precompile)
+
+  if(ARGS_DIRECTORY)
+    set(OUTPUT_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/${ARGS_DIRECTORY})
+  else()
+    set(OUTPUT_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR})
+  endif()
+  include_directories(${OUTPUT_DIRECTORY})
+
+  set(in_files "")
+  set(out_files "")
+  foreach(src ${ARGS_SOURCES} ${ARGS_UNPARSED_ARGUMENTS})
+    list(APPEND in_files "${src}")
+    string(REPLACE ${CMAKE_CURRENT_SOURCE_DIR}/  "" src ${src})
+    string(REPLACE ".vala" ".c" src ${src})
+    string(REPLACE ".gs" ".c" src ${src})
+    list(APPEND out_files "${OUTPUT_DIRECTORY}/${src}")
+  endforeach()
+
+  set(custom_vapi_arguments "")
+  if(ARGS_CUSTOM_VAPIS)
+    foreach(vapi ${ARGS_CUSTOM_VAPIS})
+      if(${vapi} MATCHES ${CMAKE_SOURCE_DIR} OR ${vapi} MATCHES ${CMAKE_BINARY_DIR})
+        list(APPEND custom_vapi_arguments ${vapi})
+      else()
+        list(APPEND custom_vapi_arguments ${CMAKE_CURRENT_SOURCE_DIR}/${vapi})
+      endif()
+    endforeach()
+  endif()
+
+  set(vapi_arguments "")
+  if(ARGS_GENERATE_VAPI)
+    list(APPEND out_files "${OUTPUT_DIRECTORY}/${ARGS_GENERATE_VAPI}.vapi")
+    set(vapi_arguments "--internal-vapi=${ARGS_GENERATE_VAPI}.vapi")
+    
+    # Header and internal header is needed to generate internal vapi
+    if (NOT ARGS_GENERATE_HEADER)
+      set(ARGS_GENERATE_HEADER ${ARGS_GENERATE_VAPI})
+    endif()
+  endif()
+
+  set(header_arguments "")
+  if(ARGS_GENERATE_HEADER)
+    list(APPEND out_files "${OUTPUT_DIRECTORY}/${ARGS_GENERATE_HEADER}.h")
+    list(APPEND out_files "${OUTPUT_DIRECTORY}/${ARGS_GENERATE_HEADER}_internal.h")
+    list(APPEND header_arguments "--header=${OUTPUT_DIRECTORY}/${ARGS_GENERATE_HEADER}.h")
+    list(APPEND header_arguments "--internal-header=${OUTPUT_DIRECTORY}/${ARGS_GENERATE_HEADER}_internal.h")
+  endif()
+
+
+  if(_Vala_Use_DEBUG)
+    message(STATUS "--------UseVala.cmake debug------------")
+    message(STATUS "Valac execute: ${VALA_EXECUTABLE} -C ${_VALA_PRECOMPILER_FLAGS} ${header_arguments} ${vapi_arguments} -b ${CMAKE_CURRENT_SOURCE_DIR} -d ${OUTPUT_DIRECTORY}  <some sources...>  ${custom_vapi_arguments}")
+    message(STATUS "--------------------")
+  endif()
+
+
+  # Note: the valac parameters -b and -d have a simple but effective action.
+  # -b (basedirectory) is removed from source filepaths, then -d
+  # (target directory) is appended to the remaining stub.
+  add_custom_command(OUTPUT ${out_files} 
+    COMMAND 
+      ${VALA_EXECUTABLE}
+    ARGS 
+      "-C"
+      ${_VALA_PRECOMPILER_FLAGS}
+      ${header_arguments} 
+      ${vapi_arguments}
+      "-b" ${CMAKE_CURRENT_SOURCE_DIR} 
+      "-d" ${OUTPUT_DIRECTORY}
+      ${in_files}
+      ${custom_vapi_arguments}
+    DEPENDS 
+      ${in_files} 
+      ${ARGS_CUSTOM_VAPIS}
+    )
+  set(${output} ${out_files} PARENT_SCOPE)
+
+endfunction()
+
